8912af71165115e62ddf1754a7409d1b3d51b19c
PraveenJJ <praveen_jj123@yahoo.com>
2024-01-25 16:05:06 +0530

    forms completed

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage>
             Center(
               child: SizedBox(
                 width: 320,
-                height: 500,
+                height: 466,
                 child: Card(
                   color: Colors.white,
                   child: Column(
@@ -96,35 +96,108 @@ class _MyHomePageState extends State<MyHomePage>
                         //   borderRadius: BorderRadius.circular(10.0), // Add rounded corners to the indicator
                         // ),
                       ),
-                      // Distance Tab Content
-                      Center(
-                        child: SingleChildScrollView(
-                          child: Padding(
-                            padding: const EdgeInsets.fromLTRB(
-                                16.0, 16.0, 16.0, 16.0),
-                            child: Form(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.stretch,
-                                children: [
-                                  buildLabeledTextField(
-                                      label: 'Enter amount spent on Fuel (Rs.)'),
-                                  buildLabeledTextField(label: 'Enter current reading of Odometer'),
-                                  buildLabeledTextField(
-                                      label: 'Enter fuel price (Rs.)'),
-                                  buildLabeledTextField(label: "Enter vehicle's mileage (kms/ltr)"),
-                                  ElevatedButton(
-                                    onPressed: () {
-                                      // Handle form submission
-                                    },
-                                    child: const Text('Calculate Distance'),
+                      Expanded(
+                        child: TabBarView(
+                          controller: _tabController,
+                          children: [
+                            // Distance Tab Content
+                            Center(
+                              child: Container(
+                                padding: const EdgeInsets.fromLTRB(
+                                    15.0, 15.0, 15.0, 10.0),
+                                child: Form(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      buildLabeledTextField(
+                                          label:
+                                              'Enter amount spent on Fuel (Rs.)'),
+                                      buildLabeledTextField(
+                                          label:
+                                              'Enter current reading of Odometer'),
+                                      buildLabeledTextField(
+                                          label: 'Enter fuel price (Rs.)'),
+                                      buildLabeledTextField(
+                                          label:
+                                              "Enter vehicle's mileage (kms/ltr)"),
+                                      Container(
+                                        padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+                                        height: 50.0,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            // Handle form submission
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.blue[
+                                                900], // Set your desired button color here
+                                            shape: const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(5)),
+                                            ),
+                                          ),
+                                          child: const Text(
+                                              'CALCULATE DISTANCE',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Colors.white)),
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                ],
+                                ),
                               ),
                             ),
-                          ),
+                            // Fuel Tab Content
+                            Center(
+                              child: Container(
+                                padding: const EdgeInsets.fromLTRB(
+                                    15.0, 15.0, 15.0, 10.0),
+                                child: Form(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.stretch,
+                                    children: [
+                                      buildLabeledTextField(
+                                          label: 'Enter travel distance (kms)'),
+                                      buildLabeledTextField(
+                                          label: 'Enter fuel price (Rs.)'),
+                                      buildLabeledTextField(
+                                          label:
+                                              "Enter vehicle's mileage (kms/ltr)"),
+                                      Container(
+                                        padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
+                                        height: 50.0,
+                                        child: ElevatedButton(
+                                          onPressed: () {
+                                            // Handle form submission
+                                          },
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.blue[
+                                            900], // Set your desired button color here
+                                            shape: const RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.all(
+                                                  Radius.circular(5)),
+                                            ),
+                                          ),
+                                          child: const Text(
+                                              'CALCULATE FUEL COST',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Colors.white)),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
                       ),
+                      // Distance Tab Content
                     ],
                   ),
                 ),
@@ -142,9 +215,9 @@ Widget buildLabeledTextField({required String label}) {
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       Text(label, style: const TextStyle(fontWeight: FontWeight.normal)),
-      const SizedBox(height: 10.0),
+      const SizedBox(height: 5.0),
       SizedBox(
-        height: 40.0, // Set the desired height here
+        height: 35.0, // Set the desired height here
         child: TextFormField(
           decoration: const InputDecoration(
             border: OutlineInputBorder(),
